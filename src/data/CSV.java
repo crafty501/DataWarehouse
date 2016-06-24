661ccddb4456f3807be189c05ed3d815b3727c4c
crafty501 <Poreski@gmx.de>
2016-06-24 22:37:14 +0200

    Third Commit

---
+++ src/data/CSV.java
@@ -0,0 +1,12 @@
+package data;
+/**
+ * Diese Klasse soll die csv-datei lesen können 
+ * und die notwendigen INSERT Queriesfür jede Zeile erstellen.
+ * Bei dieser erstellung werden Datentypen on the fly konvertiert(falls notwendig)
+ * 
+ * @author callya
+ *
+ */
+public class CSV {
+
+}
